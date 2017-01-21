62bc544b505a3701de42be3c4cbcc6842825f50f
adeto <adeto@users.noreply.github.com>
2017-01-21 13:25:56 +0200

    Bonus score

---
+++ BonusScore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06.BonusScore
+{
+    class BonusScore
+    {
+        static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            var bonScore = 0.0;
+
+
+            if (number <= 100)
+                bonScore = 5;
+
+            else 
+
+            if (number > 100)
+                bonScore = number * 0.20;
+
+            else 
+        
+            if (number > 1000)
+                bonScore = number * 0.10;
+            
+            else
+                if (number % 10 == 5)
+                    bonScore += 2;
+
+            {
+                Console.WriteLine("Bonus score: {0}", bonScore);
+            }
+
+            { 
+                Console.WriteLine("Total score: {1}", number + bonScore);
+            }
+           
+            }
+
+    }
+}
