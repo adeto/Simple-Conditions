65113a79358c56f8e100912e2c1f32cc98dc8945
adeto <adeto@users.noreply.github.com>
2017-01-21 13:32:45 +0200

    Greater Number

---
+++ GreaterNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Greater_Number
+{
+    class GreaterNumber
+    {
+        static void Main()
+        {
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber > secondNumber)
+
+            {
+                Console.WriteLine("{0}", firstNumber);
+            }
+            else 
+                    {
+                       Console.WriteLine("{0}", secondNumber);
+             }
+        }
+    }
+}
