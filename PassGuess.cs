3a2c4447d839beacb7bda1e78a72778094a0c2d0
adeto <adeto@users.noreply.github.com>
2017-01-21 13:27:05 +0200

    Guess Password

---
+++ PassGuess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10.Password_Guess
+{
+    class PassGuess
+    {
+        static void Main()
+        {
+            string pass = Console.ReadLine();
+
+            if (pass == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+
+            else
+                Console.WriteLine("Wrong password!");
+        }
+    }
+}
