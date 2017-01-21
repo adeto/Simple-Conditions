385851a0b59a925df6a62e275e98580d5c59b656
adeto <adeto@users.noreply.github.com>
2017-01-21 13:27:33 +0200

    Numbers between 100 and 200

---
+++ Number_100_And_200.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _11.Number_Betwen_100_And_200
+{
+    class Number_100_And_200
+    {
+        static void Main()
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            if (num < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+
+            else if (num >= 100 && num <= 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else if (num > 200)
+            {
+                Console.WriteLine("Greater than 200");
+            }
+        }
+    }
+}
