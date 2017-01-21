42a197c6a476f60c6f8cdaa6c8a3a4f693d29f53
adeto <adeto@users.noreply.github.com>
2017-01-21 13:26:19 +0200

    Sum Seconds

---
+++ Sum_Second.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07.SumSeconds
+{
+    class Sum_Second
+    {
+        static void Main()
+        {
+            var firstTime = int.Parse(Console.ReadLine());
+            var secondTime = int.Parse(Console.ReadLine());
+            var thirdTime = int.Parse(Console.ReadLine());
+
+        
+        }
+    }
+}
